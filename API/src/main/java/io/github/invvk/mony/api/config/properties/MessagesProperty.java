e45913c0a930c00262f0a33e316cbef57a330113
Derek <70810073+Invvk@users.noreply.github.com>
2022-05-12 09:36:58 -0700

    refactor: add message property

--- API/src/main/java/io/github/invvk/mony/api/config/properties/MessagesProperty.java
+++ API/src/main/java/io/github/invvk/mony/api/config/properties/MessagesProperty.java
@@ -1,6 +1,15 @@
 package io.github.invvk.mony.api.config.properties;
 
 import ch.jalu.configme.SettingsHolder;
+import ch.jalu.configme.properties.Property;
+import ch.jalu.configme.properties.StringProperty;
 
 public class MessagesProperty implements SettingsHolder {
+
+    public static Property<String> MOB_KILL_ACTIONBAR = new StringProperty("actionbar.mob-kill",
+            "&2+{AMOUNT} &afor killing &e{MOB}");
+
+    public static Property<String> PLAYER_COOLDOWN_TRIGGER = new
+            StringProperty("player.cooldown-started", "&aYou are now in cooldown for &e{TIME}");
+
 }
